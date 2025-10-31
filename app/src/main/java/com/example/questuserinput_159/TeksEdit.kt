ff0fa1b0ce8d4f4f0fae13a490318d2567281bcf
fikrimhammd <fikri.kdi60@gmail.com>
2025-10-31 09:21:44 +0700

    Menambahkan variabel FormDataDiri

--- app/src/main/java/com/example/questuserinput_159/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_159/TeksEdit.kt
@@ -1,3 +1,23 @@
 package com.example.questuserinput_159
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 
+@Composable
+fun FormDataDiri(modifier: Modifier) {
+    //variabel-variabel untuk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+
+    //variabel-variabel untuk menyimpan data yang diperoleh dari komponen UI
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
+
+    val gender: List<String> = listOf("Laki-laki", "Perempuan")
+}
